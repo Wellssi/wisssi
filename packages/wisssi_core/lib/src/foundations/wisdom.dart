fff099d4cc4bad7ea61d6297978b870d16500975
Kangmin An <hkkm612@gmail.com>
2025-03-11 00:49:06 +0900

    feat: add `fromJson` constructor to Wisdom

--- packages/wisssi_core/lib/src/foundations/wisdom.dart
+++ packages/wisssi_core/lib/src/foundations/wisdom.dart
@@ -3,6 +3,22 @@ import '../wisssi_core_base.dart';
 class Wisdom {
   const Wisdom({required this.name, required this.description});
 
+  factory Wisdom.fromJson(Map<String, dynamic> json) {
+    final String name = json['name'].toString();
+    final Map<String, dynamic> rawDescription =
+        json['description'] as Map<String, dynamic>;
+
+    final Map<LanguageType, String> description = <LanguageType, String>{};
+    for (final String key in rawDescription.keys) {
+      final LanguageType? languageType = LanguageType.fromString(key);
+      if (languageType != null) {
+        description[languageType] = rawDescription[key].toString();
+      }
+    }
+
+    return Wisdom(name: name, description: description);
+  }
+
   final String name;
   final Map<LanguageType, String> description;
 }
